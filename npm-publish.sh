b6e6c9ca6d43a3c2914cc80361f54cf0874acb06
Iven Schlenther <iven.s@lvckyworld.net>
2024-02-16 16:50:47 +0100

    [TASK] dont publish npm if same version

---
+++ npm-publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get the current version from package.json
+current_version=$(node -p "require('./package.json').version")
+
+# Get the latest published version from npm registry
+latest_version=$(npm show @lvckyworld/marina-api version)
+
+# Compare versions
+if [ "$current_version" != "$latest_version" ]; then
+  echo "Versions are different. Proceeding with publishing..."
+  npm publish --access public
+else
+  echo "Version $current_version is already published. Skipping publishing."
+fi
