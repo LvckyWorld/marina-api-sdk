e713c775df7bb2298cfd526722cc3edc00901a71
Iven Schlenther <iven.s@lvckyworld.net>
2024-03-13 23:14:19 +0100

    [!!!][TASK] implement new endpoints
    BREAKING:
    - new endpoints
    - new datas
    - new functions
    - new auth method

---
+++ src/core/requestTypes/LWGetRequest.ts
@@ -0,0 +1,104 @@
+import {LvckyWorldRequest} from "../../contract/abstract/LvckyWorldRequest";
+import {DiscordUserData, GlobalBan, GlobalBans, TeamMember} from "../../contract/ApiData";
+import * as axios from "axios";
+import {Snowflake} from "discord-api-types/globals";
+
+/**
+ * Represents a class for making put requests to the LvckyWorld API.
+ */
+export class LWGetRequest extends LvckyWorldRequest {
+
+    /**
+     * Creates an instance of the constructor.
+     * @param {string} apiUrl - The URL of the API to be used.
+     * @constructor
+     */
+    constructor(private apiUrl: string) {
+        super();
+    }
+
+    /**
+     * Retrieves global bans from the specified API endpoint.
+     * @returns {Promise<GlobalBans>} A promise that resolves to the response data containing global bans.
+     */
+    public async getGlobalBans(): Promise<GlobalBans> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await this.sendGetRequest(this.apiUrl, '/globalbans');
+                if (!response.success) return reject(response.error);
+                return resolve(response.data as GlobalBans);
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }
+
+    /**
+     * Retrieves the global ban information for a given member.
+     * @param {Snowflake} memberId - The ID of the member whose global ban information is to be retrieved.
+     * @return {Promise<GlobalBan>} A promise that resolves with the global ban information (if found) or rejects with an error.
+     */
+    public getGlobalBan(memberId: Snowflake): Promise<GlobalBan> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await axios.default.get(this.apiUrl + '/globalbans/' + memberId);
+                return resolve(response.data as GlobalBan);
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }
+
+    /**
+     * Retrieves the LvckyWorld team members from the API.
+     * @returns {Promise<TeamMember[]>} A promise that resolves with the LvckyWorld team members.
+     * @throws {Error} If there was an error while retrieving the team members.
+     */
+    public async getLvckyWorldMembers(): Promise<TeamMember[]> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await axios.default.get(this.apiUrl + '/team-members');
+                return resolve(response.data as TeamMember[]);
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }
+
+    /**
+     * Retrieves the list of system administrators.
+     * @returns {Promise<TeamMember[]>} A promise that resolves with an array of system administrators (TeamMember objects).
+     * @throws {any} If an error occurs while fetching the data.
+     */
+    public async getSystemAdmins(): Promise<TeamMember[]> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await this.sendGetRequest(this.apiUrl, '/team-members');
+                if (!response.success) return reject(response.error);
+                const data: TeamMember[] = response.data as TeamMember[];
+                return resolve(data.filter(((member: TeamMember) => member.is_admin)) as TeamMember[]);
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }
+
+    /**
+     * Retrieves user information from the server using the provided member ID.
+     * @param {string} memberId - The unique identifier of the member.
+     * @return {Promise<DiscordUserData>} - A promise that resolves with the retrieved DiscordMember object.
+     * @throws {Error} - If an error occurs during the retrieval process.
+     */
+    public async getUserInfo(memberId: Snowflake): Promise<DiscordUserData> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await this.sendGetRequest(this.apiUrl, '/users/' + memberId);
+                if (!response.success) return reject(response.error);
+                return resolve(response.data as DiscordUserData);
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }
+
+}
---
+++ src/core/requestTypes/PutRequest.ts
@@ -0,0 +1,80 @@
+import {LvckyWorldRequest} from "../../contract/abstract/LvckyWorldRequest";
+import {GlobalBan} from "../../contract/ApiData";
+
+/**
+ * Represents a class for making put requests to the LvckyWorld API.
+ */
+export class LWPutRequests extends LvckyWorldRequest {
+
+    constructor(private apiUrl: string, private apiToken: string) {
+        super();
+    }
+
+
+    /**
+     * Edits a global ban.
+     *
+     * @param {string | null} banReason - The reason for the ban. Defaults to null.
+     * @param {string | null} specialPermit - The special permit for the ban. Defaults to null.
+     * @returns {Promise<GlobalBan>} - A Promise that resolves with the edited global ban.
+     * @throws {Error} - If the request fails or an error occurs.
+     */
+    public async editGlobalBan(
+        banReason: string | null = null,
+        specialPermit: string | null = null,
+    ): Promise<GlobalBan> {
+
+        let params = {
+            ban_reason: banReason,
+            special_permit: specialPermit
+        }
+
+        const request = await this.sendPostRequest(this.apiUrl, '/globalbans', this.apiToken, params);
+
+        if (request.success) {
+            return request.data as GlobalBan;
+        }
+        throw request.error ?? request;
+    }
+
+    /**
+     * Changes the ban reason for a global ban.
+     *
+     * @param {string} banReason - The new ban reason.
+     * @return {Promise<GlobalBan>} - A promise that resolves to the updated GlobalBan object.
+     * @throws {any} - Throws the error object returned by the API or the original request object if no error is provided.
+     */
+    public async changeBanReason(
+        banReason: string
+    ): Promise<GlobalBan> {
+        const request = await this.sendPutRequest(this.apiUrl, '/globalbans', this.apiToken, {
+            ban_reason: banReason
+        });
+
+        if (request.success) {
+            return request.data as GlobalBan;
+        }
+        throw request.error ?? request;
+    }
+
+    /**
+     * Changes the special permit status of a global ban.
+     *
+     * @param {boolean} specialPermit - The new special permit status. True to enable, false to disable.
+     * @returns {Promise<GlobalBan>} - A promise that resolves to the updated global ban object.
+     * @throws {Error} - An error is thrown if the request fails or if the response is unsuccessful.
+     */
+    public async changeSpecialPermit(
+        specialPermit: boolean
+    ): Promise<GlobalBan> {
+        const request = await this.sendPutRequest(this.apiUrl, '/globalbans', this.apiToken, {
+            special_permit: specialPermit
+        });
+
+        if (request.success) {
+            return request.data as GlobalBan;
+        }
+        throw request.error ?? request;
+    }
+
+}
